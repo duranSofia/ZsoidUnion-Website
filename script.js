fa1fb92984c81703a315629a9881af533134f9b9
Duran Sofia <durannsofia@gmail.com>
2020-10-13 07:27:36 +0200

    final changes

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// const navBar = document.querySelector(".navbar")
-// const hamburgerIcon = document.querySelector(".hamburger-icon")
-
-// hamburgerIcon.addEventListener("click",toggleHamburger);
-
-// function toggleHamburger(){
-//   navBar.classList.toggle("showNav")
-//   hamburgerIcon.classList.toggle("showClose")
-// }
-
-// const menuLinks = document.querySelectorAll(".menuLink")
-// menuLinks.forEach(
-//   function(menuLinks){
-//     menuLinks.addEventListener("click",toggleHamburger)
-//   }
-// )
-
 function buttonToggle(x) {
   x.classList.toggle("toggle-button-change");
   
